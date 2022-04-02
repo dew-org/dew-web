b95fed20ae84daf0e9d7233d86bb0546689bc3be
Carlos Pavajeau <cantte098@gmail.com>
2022-04-02 10:19:37 -0500

    feat(products): add products registration

---
+++ apps/web/src/pages/api/products/index.ts
@@ -0,0 +1,28 @@
+import { ProductService } from '@dew-org/products'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+const handler = async (request: NextApiRequest, response: NextApiResponse) => {
+  if (request.method === 'POST') {
+    const { body } = request
+
+    try {
+      await ProductService.save(body)
+      response.status(201).json({ message: 'Product saved' })
+    } catch (error) {
+      if (error.response) {
+        response.status(error.response.status).json({
+          message: error.response.data.message,
+        })
+      } else {
+        response.status(500).json({ message: error.message })
+      }
+    }
+
+    return
+  }
+
+  // Method not allowed
+  response.status(405).end()
+}
+
+export default handler
