66f60e69a9716e14b19ab4701996f71629dc2389
Carlos Pavajeau <cantte098@gmail.com>
2022-04-02 09:32:50 -0500

    feat: add intl configuration in next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,8 +1,24 @@
+// @ts-check
 const withTM = require('next-transpile-modules')([
   '@dew-org/shared',
   '@dew-org/products',
 ])
 
+/**
+ * @type {import('next').NextConfig}
+ **/
 module.exports = withTM({
   reactStrictMode: true,
+  i18n: {
+    locales: ['en', 'es'],
+    defaultLocale: 'en',
+  },
+  webpack(config, { dev, ...other }) {
+    if (!dev) {
+      // https://formatjs.io/docs/guides/advanced-usage#react-intl-without-parser-40-smaller
+      config.resolve.alias['@formatjs/icu-messageformat-parser'] =
+        '@formatjs/icu-messageformat-parser/no-parser'
+    }
+    return config
+  },
 })
