ac0e2199b14051c4ac2735836ca0a32c6cc1a296
Carlos Pavajeau <cantte098@gmail.com>
2022-04-04 08:47:06 -0500

    feat(products): add use product hook

---
+++ packages/products/src/hooks/use-product.ts
@@ -0,0 +1,17 @@
+import { fetcher } from '@dew-org/shared'
+import useSWR from 'swr'
+
+import { Product } from '../types'
+
+const useProduct = (
+  code: string,
+): [Product | undefined, boolean, Error | undefined] => {
+  const { data, isValidating, error } = useSWR<Product>(
+    `/api/products/${code}`,
+    fetcher,
+  )
+
+  return [data, isValidating, error]
+}
+
+export default useProduct
