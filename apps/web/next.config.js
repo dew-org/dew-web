9adb117071a5a1b78b483653a2dada00e2560392
Carlos Pavajeau <cantte098@gmail.com>
2022-04-19 16:07:10 -0500

    feat(web): include inventory module

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,6 +3,7 @@ const withTM = require('next-transpile-modules')([
   '@dew-org/shared',
   '@dew-org/customers',
   '@dew-org/catalogue',
+  '@dew-org/inventory',
   '@dew-org/products',
   '@dew-org/invoices',
 ])
