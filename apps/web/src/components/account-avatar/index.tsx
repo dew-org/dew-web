a1d398440d56830087ad0794cd0522bd3efdf35c
Carlos Pavajeau <cantte098@gmail.com>
2022-04-27 20:45:18 -0500

    feat: new account avatar

--- apps/web/src/components/account-avatar/index.tsx
+++ apps/web/src/components/account-avatar/index.tsx
@@ -1,35 +1,63 @@
 import { useUser } from '@auth0/nextjs-auth0'
 import {
-  Link,
+  Button,
+  Divider,
+  Grid,
   Loading,
+  Popover,
   Row,
-  Spacer,
   Text,
-  Tooltip,
   User,
 } from '@nextui-org/react'
-import { useMemo } from 'react'
+import NextLink from 'next/link'
+import { Logout, Setting, User as UserIcon } from 'react-iconly'
 import { FormattedMessage } from 'react-intl'
 
 const AccountAvatar = () => {
   const { user, isLoading } = useUser()
 
-  const tooltipContent = useMemo(
-    () => (
-      <>
-        <Text h5>{user?.name}</Text>
+  const content = (
+    <Grid.Container
+      css={{
+        mw: '270px',
+        borderRadius: '$lg',
+        padding: '$sm',
+      }}
+    >
+      <Grid xs={12} direction="column">
+        <Text h4>{user?.name}&nbsp;</Text>
         <Text small>{user?.email}</Text>
+      </Grid>
 
-        <Spacer y={1} />
+      <Divider y={1} />
 
-        <Row justify="center">
-          <Link color="error" href="/api/auth/logout" block>
+      <Grid xs={12}>
+        <Grid.Container gap={1}>
+          <Grid xs={12} direction="column">
+            <Button light icon={<UserIcon />} size="sm">
+              <FormattedMessage defaultMessage="Profile" />
+              <FormattedMessage defaultMessage="Heelo" />
+            </Button>
+          </Grid>
+
+          <Grid xs={12} direction="column">
+            <Button light icon={<Setting />} size="sm">
+              <FormattedMessage defaultMessage="Settings" />
+            </Button>
+          </Grid>
+        </Grid.Container>
+      </Grid>
+
+      <Divider y={1} />
+
+      <Row justify="flex-end">
+        <NextLink href="/api/auth/logout" passHref>
+          <Button auto color="error" icon={<Logout />}>
             <FormattedMessage defaultMessage="Logout" />
-          </Link>
-        </Row>
-      </>
-    ),
-    [user],
+          </Button>
+        </NextLink>
+      </Row>
+    </Grid.Container>
   )
 
   return (
@@ -37,15 +65,31 @@ const AccountAvatar = () => {
       {isLoading && <Loading />}
 
       {user && (
-        <Tooltip
-          trigger="click"
-          placement="bottomEnd"
-          content={tooltipContent}
-          shadow
-          css={{ zIndex: '$max' }}
-        >
-          <User name={user.name} src={user.picture} pointer />
-        </Tooltip>
+        <Popover placement="bottom-left">
+          <Popover.Trigger>
+            <User
+              name={user.name}
+              src={user.picture}
+              pointer
+              as="button"
+              css={{
+                px: 0,
+                transition: 'opacity 0.25s ease',
+                cursor: 'pointer',
+                borderRadius: '$xs',
+                background: 'none',
+                border: 'none',
+                '&:hover': {
+                  opacity: 0.8,
+                },
+              }}
+            />
+          </Popover.Trigger>
+
+          <Popover.Content css={{ px: '$4', py: '$2' }}>
+            {content}
+          </Popover.Content>
+        </Popover>
       )}
     </>
   )
