2138226e1f7b6da664a5baf49e337e70334d5051
Carlos Pavajeau <cantte098@gmail.com>
2022-05-05 10:35:09 -0500

    feat: set Inter as default font

--- apps/web/src/theme/index.ts
+++ apps/web/src/theme/index.ts
@@ -1,7 +1,7 @@
 import { createTheme } from '@nextui-org/react'
 
 const fonts = {
-  sans: '-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif',
+  sans: 'Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif',
   mono: 'Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace',
 }
 
