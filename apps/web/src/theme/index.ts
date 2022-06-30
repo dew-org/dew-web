a24e7b4df8eb051b3b724198e0b4050653e8a90b
Carlos Pavajeau <cantte098@gmail.com>
2022-06-30 11:23:24 -0500

    refactor: update default font to Open Sans

--- apps/web/src/theme/index.ts
+++ apps/web/src/theme/index.ts
@@ -1,7 +1,7 @@
 import { createTheme } from '@nextui-org/react'
 
 const fonts = {
-  sans: 'Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif',
+  sans: '"Open Sans", -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif',
   mono: 'Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace',
 }
 
