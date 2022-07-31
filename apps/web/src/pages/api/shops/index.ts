8d719214339b20d41f97788a832f90ef3a28471f
Carlos Pavajeau <cantte098@gmail.com>
2022-07-30 19:29:09 -0500

    feat(shops): add find by user api endpoint

--- apps/web/src/pages/api/shops/index.ts
+++ apps/web/src/pages/api/shops/index.ts
@@ -1,4 +1,4 @@
-import { withApiAuthRequired } from '@auth0/nextjs-auth0'
+import { getSession, withApiAuthRequired } from '@auth0/nextjs-auth0'
 import { ShopService } from '@dew-org/shops/src'
 import withBearerToken from '@dew-org/utils/api/auth/with-bearer-token'
 import withErrorHandler from '@dew-org/utils/api/with-error-handler'
@@ -11,6 +11,16 @@ const handler = async (request: NextApiRequest, response: NextApiResponse) => {
     await ShopService.save(body)
 
     response.status(201).json({ message: 'success' })
+
+    return
+  }
+
+  if (request.method === 'GET') {
+    const { user } = getSession(request, response)
+
+    const shop = await ShopService.findByUserId(user.sub)
+
+    response.status(200).json(shop)
 
     return
   }
