3e61a90da7f793f8e77af696a147ba74b2312f7e
Carlos Pavajeau <cantte098@gmail.com>
2022-10-26 14:38:35 -0500

    feat: add api routes

---
+++ apps/web/src/pages/api/analytics/[date]/[frequency]/index.ts
@@ -0,0 +1,26 @@
+import { getSession, withApiAuthRequired } from '@auth0/nextjs-auth0'
+import { AnalyticsService } from '@dew-org/analytics'
+import withBearerToken from '@dew-org/utils/api/auth/with-bearer-token'
+import withErrorHandler from '@dew-org/utils/api/with-error-handler'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+const handler = async (request: NextApiRequest, response: NextApiResponse) => {
+  if (request.method === 'GET') {
+    const { user } = await getSession(request, response)
+    const { date, frequency } = request.query
+
+    const analytics = await AnalyticsService.fetch(
+      date as string,
+      frequency as string,
+      user.sub,
+    )
+
+    response.status(200).json(analytics)
+    return
+  }
+
+  // Method not allowed
+  response.status(405).end()
+}
+
+export default withApiAuthRequired(withErrorHandler(withBearerToken(handler)))
